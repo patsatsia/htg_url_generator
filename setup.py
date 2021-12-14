a4a99e79889bd88a55056f5b926e9f674369ef07
giorgi_pats <giorgi.patsatsia@makingscience.com>
2021-12-14 17:56:56 +0400

    Change version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='htg_url_generator',
-    version='0.1.1',
+    version='0.1.2',
     packages=['htg_url'],
     long_description_content_type='text/markdown',
 )
