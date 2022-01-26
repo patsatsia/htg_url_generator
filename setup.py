b125affa00cf8543c52936ab3154f31c9319e68f
giorgi_pats <giorgi.patsatsia@makingscience.com>
2022-01-26 17:52:58 +0400

    Update package version to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='htg_url_generator',
-    version='0.1.6',
+    version='0.1.7',
     packages=['htg_url'],
     long_description_content_type='text/markdown',
 )
